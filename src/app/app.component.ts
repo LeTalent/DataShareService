3c46d495c10fdf19e67eb49504805b459a11234d
Maurice <MauriceNgassa.Happi@antrago.de>
2019-10-07 09:21:19 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Exp1-from-input-to-component';
+}
